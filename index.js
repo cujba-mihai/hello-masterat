5c3df2c851fe81444274fba719bab6fd3df665b3
Mihai <mihai.cujba@indrivo.com>
2022-10-03 14:30:51 +0300

    Removed unnecessary scrips

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sanitizeHtml = require('sanitize-html');
-const { scriptsToAdd, mainStyles } = require('./tags');
+const { mainStyles } = require('./tags');
 
 const fs = require('fs');
 const htmlBefore = fs.readFileSync('./index.html', { encoding: 'utf8' });
@@ -11,15 +11,67 @@ const htmlAfter = sanitizeHtml(htmlBefore, {
     },
     allowedAttributes: {
         '*': ['id', 'src', 'crossorigin', 'integrity', 'alt', 'type', 'disabled', 'checked', 'value']
-    }
+    },
+    allowVulnerableTags: true
+
 });
 
-const withScripts = (html) => html.replace('</body>', scriptsToAdd.join('').concat('</body>'));
 const withStyle = html => html.replace('</head>', mainStyles.join('').concat('</head>'));
 
-const sanitizeHTML = html => withScripts(withStyle(html));
+const sanitizeHTML = html => withStyle(html);
+
+
 
 const cleanHtml = require('clean-html').clean(htmlAfter, undefined, (html) => {
-    return fs.writeFileSync('./index.html', new Buffer.from(sanitizeHTML(html)));
-});
+    const { parse } = require('node-html-parser');
+    const root = parse(html);
 
+    const questionContainers = root.querySelectorAll('.deferredfeedback');
+    const questions = root.querySelectorAll('.qtext');
+    const grades = root.querySelectorAll('.state + .grade');
+    const pageWrappers = root.querySelectorAll('#page-wrapper');
+
+    for (let i = 0; i < pageWrappers.length; i++) {
+        if (i === 0) {
+            pageWrappers[i].style = 'padding-top: 50px;'
+        } else {
+            pageWrappers[i].style = 'padding-top: 0px;'
+        }
+    }
+
+
+    const uniqueQuestions = {};
+
+    for (let i = 0; i < questions.length; i++) {
+        const question = questions[i].innerText;
+        const gradeText = grades[i].innerText?.replace(/Marked out/g, '0.00');
+        const questionContainer = questionContainers[i];
+        const grade = gradeText
+            ?.replace(/,/g, '.')
+            .split(' ')
+            .filter(grade => parseFloat(grade) >= 0);
+
+        const rank = grade?.reduce((a, b) => parseFloat(a) / parseFloat(b));
+
+        if (uniqueQuestions?.[question] <= rank) {
+            questionContainer && questionContainer.setAttribute('display', 'none');
+            uniqueQuestions[question] = parseFloat(rank);
+        } else {
+            uniqueQuestions[question] = parseFloat(rank);
+        }
+
+        if (questionContainer) {
+            switch (rank) {
+                case 1:
+                    questionContainer.setAttribute('style', 'background-color: #a1d5a1;');
+                    break;
+                case 0:
+                    questionContainer.setAttribute('style', 'background-color: #fdccd1;');
+                    break;
+                default:
+                    questionContainer.setAttribute('style', 'background-color: #f4e7a1;');
+            }
+        }
+    }
+    return fs.writeFileSync('./index.html', new Buffer.from(sanitizeHTML(root.toString())));
+});
